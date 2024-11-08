c738f1a463223f3483c1af1734abbbbc574c836e
Oche1-deals <odohraphaeloche11@gmail.com>
2024-11-08 02:41:19 +0100

    versioning

---
+++ restful-web-service/src/main/java/com/oche/rest/webservices/restful_web_service/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.oche.rest.webservices.restful_web_service.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
