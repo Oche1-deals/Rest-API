e64f619a4bec00cc38f831109b31eeed2a3d1712
Oche1-deals <odohraphaeloche11@gmail.com>
2024-11-13 00:32:49 +0100

    jpa&hibernate

--- restful-web-service/src/main/java/com/oche/rest/webservices/restful_web_service/user/User.java
+++ restful-web-service/src/main/java/com/oche/rest/webservices/restful_web_service/user/User.java
@@ -1,12 +1,18 @@
 package com.oche.rest.webservices.restful_web_service.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2 , message = "Name should have at least two characters")
     @JsonProperty("user_name")
---
+++ restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(birth_date,id,name)
+values(current_date(),10001,'Ranga');
+
+insert into user_details(birth_date,id,name)
+values(current_date(),10002,'Ravi');
+
+insert into user_details(birth_date,id,name)
+values(current_date(),10003,'sathish');
